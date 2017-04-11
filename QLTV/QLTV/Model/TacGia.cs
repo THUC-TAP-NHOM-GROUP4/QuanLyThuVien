4866557645631ee1251ce953a3ec3dfdeea3d296
doanthanhhuong <huongthanhlan@gmail.com>
2017-04-11 21:59:09 +0700

    chinh sua giao dien

---
+++ QLTV/QLTV/Model/TacGia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLTV.Model
+{
+    class TacGia
+    {
+        public  string ma { get; set; }
+        public string ten { get; set; }
+    }
+}
